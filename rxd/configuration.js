ff290527ab1084f3a7dedb1324785222341c7245
wimbervoetsatgroupjansen <wim.bervoets@groupjansen.com>
2022-06-17 12:13:53 +0200

    navision

---
+++ rxd/configuration.js
@@ -0,0 +1,17 @@
+var RXDConfig = {
+    // the entry URL to the content
+    contentUrl: "https://author.eloomi.com/lms-preview?courseId=21bbee26-e6e1-49d7-bcd4-b9d0cb826201",
+
+    // value to use as the HTML title for the proxy package
+    courseTitle: "Navision  introductie",
+
+    // URL where the RXD files are located and available via http/s
+    rxdHostUrl: "https://author.eloomi.com/assets/scorm/",
+
+    // the path to the content API wrapper page, appended to the rxdHostUrl
+    // unless it is an absolute http/s URL
+    contentApi: "https://author.eloomi.com/scormContentAPI",
+
+    // URL to fetch pre-launch configuration settings from
+    preLaunchConfigurationUrl: ""
+};
